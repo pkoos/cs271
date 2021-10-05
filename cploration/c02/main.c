5db21afd5a6179d80a5121546066f8bd6733ab84
Paul Koos <paully_k5298@yahoo.com>
2021-10-05 08:24:08 -0700

    complete exercise 1

---
+++ cploration/c02/main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#define MAX_LIMIT 100
+
+int main() {
+/** WARMUP **/
+// declare a string with value
+char hello[] = "Hello";
+// declare an empty string
+char name[MAX_LIMIT];
+
+// prompt user
+printf("What is your name? ");
+
+// read in a string from user and save in variable
+// [^\n] means to discard the newline character
+scanf("%[^\n]s", name);
+
+// print out "hello <name>"
+printf("%s %s!\n\n", hello, name);
+
+/** Exercise 1 **/ 
+char welcome[MAX_LIMIT];
+strcpy(welcome, hello);
+strcat(welcome, " ");
+strcat(welcome, name);
+printf("%s!\n", welcome);
+    return 0;
+}
