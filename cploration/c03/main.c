1ee470758e3081eef6452d6852d3cb5b28df628c
Paul Koos <paully_k5298@yahoo.com>
2021-10-13 08:57:47 -0700

    add logic to check correct number of command line arguments and verify if file was opened successfully.

--- cploration/c03/main.c
+++ cploration/c03/main.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LENGTH 200
+
 int main(int argc, const char *argv[]) {
+    if(argc != 2) {
+        printf("Usage: %s [filename]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    FILE *fin = fopen(argv[1], "r");
+    if(fin == NULL) {
+        perror("Unable to open file!");
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
