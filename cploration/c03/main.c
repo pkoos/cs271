04943e1bf18c2c6e493a28d5de79314c8f0bcbd9
Paul Koos <paully_k5298@yahoo.com>
2021-10-13 11:10:29 -0700

    correct segmentation fault and add loop to copy to file

--- cploration/c03/main.c
+++ cploration/c03/main.c
@@ -16,10 +16,19 @@ int main(int argc, const char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    char *out_file = "";
+    char out_file[35];
     strcpy(out_file, argv[1]);
     strcat(out_file, ".echo");
     FILE *fout = fopen(out_file, "w+");
+
+    char line[MAX_LINE_LENGTH] = {0};
+    unsigned int line_num = 0;
+
+    while(fgets(line, sizeof(line), fin)) {
+        line_num ++;
+        printf("[%04d] %s", line_num, line);
+        fprintf(fout, "%s", line);
+    }
 
     return 0;
 }
