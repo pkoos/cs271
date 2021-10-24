45a840a7ddf509f5e9269034662df94f7b83eba4
Paul Koos <paully_k5298@yahoo.com>
2021-10-24 11:59:56 -0700

    commit first working version of code

--- cploration/c04/parser.c
+++ cploration/c04/parser.c
@@ -5,18 +5,17 @@ void parse(FILE * file) {
     // unsigned int line_num = 0;
 
     while(fgets(line, sizeof(line), file)) {
-        strip(*line);
+        strip(line);
         if(!*line) { continue; }
-        // line_num++;
         printf("%s", line);
     }
 }
 
 char *strip(char *s) {
-    char s_new[sizeof s / sizeof s[0]] = {0};
+    char s_new[50] = {0};
     int i = 0;
     for(char *s2 = s; *s2; s2++) {
-        if(s == '/' && *(s+1) == '/') { 
+        if(*s == '/' && *(s+1) == '/') { 
             break;
         } else if(!isspace(*s)) {
             s_new[i++] = *s2;
