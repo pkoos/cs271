99b26ed51a579a35db9a08b5c68255cbb7802629
Paul Koos <paully_k5298@yahoo.com>
2021-10-31 15:10:16 -0700

    complete is_Atype function

---
+++ cploration/c05/parser.c
@@ -0,0 +1,37 @@
+#include "parser.h"
+
+void parse(FILE * file) {
+    char line[MAX_LINE_LENGTH] = {0};
+    
+
+    while(fgets(line, sizeof(line), file)) {
+        char inst_type = ' ';
+        strip(line);
+        if(!*line) { continue; }
+        if(is_Atype(line)) {
+            inst_type = 'A';
+        }
+        printf("%c  %s\n",inst_type, line);
+    }
+}
+
+char *strip(char *s) {
+    char s_new[strlen(s) + 1];
+    int i = 0;
+    
+    for(char *s2 = s; *s2; s2++) {
+        if(*s == '/' && *(s+1) == '/') { 
+            break;
+        } else if(!isspace(*s2)) {
+            s_new[i++] = *s2;
+        }
+    }
+    s_new[i] = '\0';
+    strcpy(s, s_new);
+    
+    return s;
+}
+
+bool is_Atype(const char *line) {
+    return strncmp("@", line, 1) == 0;
+}
