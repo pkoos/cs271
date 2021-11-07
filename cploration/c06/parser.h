4547ddeb05fccbbe5d15c69380b7bd522eebab23
Paul Koos <paully_k5298@yahoo.com>
2021-11-06 23:01:45 -0700

    add structs to parser.h file

--- cploration/c06/parser.h
+++ cploration/c06/parser.h
@@ -10,6 +10,33 @@
 
 typedef int16_t hack_addr;
 typedef int16_t opcode;
+typedef struct c_instruction {
+    opcode a:1;
+    opcode comp:6;
+    opcode dest:3;
+    opcode jump:3;
+} c_instruction;
+
+typedef struct a_instruction {
+    union address {
+        hack_addr address;
+        char * label;
+    } address;
+    bool is_addr;
+} a_instruction;
+
+typedef struct instruction {
+    union instr_type {
+        a_instruction a_inst;
+        c_instruction c_inst;
+    } instr_type;
+} instruction ;
+
+enum instruction_type {
+    invalid = -1,
+    a_type,
+    c_type
+};
 
 char *strip(char *);
 void parse(FILE *);
