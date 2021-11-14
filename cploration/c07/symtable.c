c8e2831a47dbb81ed00feef3c207108ec52dc9a7
Paul Koos <paully_k5298@yahoo.com>
2021-11-13 22:07:05 -0800

    implement hash function for exercise 3

--- cploration/c07/symtable.c
+++ cploration/c07/symtable.c
@@ -1,1 +1,11 @@
 #include "symtable.h"
+
+int hash(char *str) {
+    unsigned int hash = 5381;
+    int c;
+
+    while( (c = *str++) ) {
+        hash = ((hash << 5) + hash) + c;
+    }
+    return hash;
+}
