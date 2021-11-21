5ca5eaf3fc59c0763cdedc24eba853a71a0cb8eb
Paul Koos <paully_k5298@yahoo.com>
2021-11-20 20:16:57 -0800

    initial commit for cploration8

---
+++ cploration/c08/parser.c
@@ -0,0 +1,70 @@
+#include "parser.h"
+
+void parse(FILE * file) {
+    char line[MAX_LINE_LENGTH] = {0};
+    
+
+    while(fgets(line, sizeof(line), file)) {
+        char inst_type = ' ';
+        strip(line);
+        if(!*line) { continue; }
+        if(is_Atype(line)) {
+            inst_type = 'A';
+        } else if(is_label(line)) {
+            inst_type = 'L';
+            char label[MAX_LABEL_LENGTH];
+            extract_label(line, label);
+            strcpy(line, label);
+        } else if(is_Ctype(line)) {
+            inst_type = 'C';
+        }
+        printf("%c  %s\n",inst_type, line);
+    }
+}
+
+char *strip(char *s) {
+    char s_new[strlen(s) + 1];
+    int i = 0;
+    
+    for(char *s2 = s; *s2; s2++) {
+        if(*s2 == '/' && *(s2+1) == '/') { 
+            break;
+        } else if(!isspace(*s2)) {
+            s_new[i++] = *s2;
+        }
+    }
+    s_new[i] = '\0';
+    strcpy(s, s_new);
+    
+    return s;
+}
+
+char *extract_label(const char* line, char * label) {    
+    int i = 0;
+    for(const char *s2 = line;*s2;s2++) {
+        if(*s2 == '(' || *s2 == ')') {
+            continue;
+        } else {
+            label[i++] = *s2;
+        }
+    }
+    label[i] = '\0';
+
+    return label;
+}
+
+bool is_Atype(const char *line) {
+    return strncmp("@", line, 1) == 0;
+}
+
+bool is_label(const char *line) {
+    if(line[0] != '(' && line[strlen(line) -1] != ')') {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+bool is_Ctype(const char *line) {
+    return !is_Atype(line) && !is_label(line);
+}
---
+++ cploration/c08/symtable.h
@@ -0,0 +1,18 @@
+#include<stdint.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+#define SYMBOL_TABLE_SIZE 100
+
+typedef int16_t hack_addr;
+typedef struct Symbol {
+    char *name;
+    hack_addr address;
+} Symbol;
+
+Symbol* hashArray[SYMBOL_TABLE_SIZE];
+
+int hash(char *);
+Symbol *find(char *);
+void insert(char *, hack_addr);
+void display_table();
