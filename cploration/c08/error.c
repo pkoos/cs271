417268c6b654a171f6f0217409e3d3a94e1895cc
Paul Koos <paully_k5298@yahoo.com>
2021-11-20 20:36:04 -0800

    add function and error messages to error header and file

---
+++ cploration/c08/error.c
@@ -0,0 +1,18 @@
+#include "error.h"
+
+const char *error_messages[] = 
+{
+    [EXIT_INCORRECT_ARGUMENTS] = "Usage %s [filename]",
+};
+
+void exit_program(enum exitcode code, ...) {
+    va_list arguments;
+    va_start(arguments, code);
+
+    printf("ERROR: ");
+    vfprintf(stdout, error_messages[code], arguments);
+    printf("\n");
+
+    va_end(arguments);
+    exit(code);
+}
--- cploration/c08/error.h
+++ cploration/c08/error.h
@@ -2,6 +2,8 @@
 #define __ERROR_H__
 
 #include<stdarg.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 enum exitcode {
     EXIT_INCORRECT_ARGUMENTS = 1,
