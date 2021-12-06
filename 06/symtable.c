3691b1fd47ee3a8783067aaee403fe88d20d4afd
Paul Koos <paully_k5298@yahoo.com>
2021-12-06 10:19:38 -0800

    remove debugging strings and comments from symtable

--- 06/symtable.c
+++ 06/symtable.c
@@ -15,9 +15,7 @@ void symtable_insert(char *name, hack_addr value) {
     item->address = value;
     item->name = (char *) malloc(strlen(name) + 1);
     strcpy(item->name, name);
-    // item->name = name;
     int hashIndex = hash(name);
-    // printf("Before added\nitem address: %d, item name: %s, length: %lu\n", item->address, item->name, strlen(item->name));
 
     while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->name != NULL) {
         ++hashIndex;
@@ -26,18 +24,12 @@ void symtable_insert(char *name, hack_addr value) {
     }
 
     hashArray[hashIndex] = item;
-    // printf("Before free\nCollection address: %d, collection name: %s\n", hashArray[hashIndex]->address, hashArray[hashIndex]->name);
-
-    // free(item);
-    // printf("After added\nCollection address: %d, collection name: %s\n", hashArray[hashIndex]->address, hashArray[hashIndex]->name);
 }
 
 Symbol *symtable_find(char *name) {
-    // printf("Name Argument: %s, length: %lu\n", name, strlen(name));
     int hashIndex = hash(name);
 
     while(hashArray[hashIndex] != NULL) {
-        // printf("Argument: %s, symtable: %s\n", name, hashArray[hashIndex]->name);
         if(strcmp(hashArray[hashIndex]->name, name) == 0) {
             return hashArray[hashIndex];
         }
@@ -54,7 +46,8 @@ void symtable_display_table() {
 
     for(i = 0; i < SYMBOL_TABLE_SIZE; i++) {
         if(hashArray[i] != NULL) {
-            printf(" (%s, %d) ",hashArray[i]->name, hashArray[i]->address);        }
+            printf(" (%s, %d) ", hashArray[i]->name, hashArray[i]->address);
+        }
         else {
             printf(" ~~ ");
         }
